527e9a13fb2e950721ee564b9cdd5cee2145a097
Alex <al8891412@gmail.com>
2023-04-26 20:26:04 -0400

    owo

---
+++ Human.java
@@ -0,0 +1,28 @@
+public Human{
+    
+    //Instance Variables
+    int height;
+    double weight;
+
+
+    //Class constructor
+    public Human(int h, double w){
+        height = h;
+        wieght = w;
+    }
+
+    //Default no argument constructor
+    public Human(){
+        height = 100;
+        weight = 50;
+    }
+
+    //Class methods
+    public void doSomething(){
+        System.out.println("I did something.");
+    }
+
+    public void eat(){
+        //Code for eating...
+    }
+}
